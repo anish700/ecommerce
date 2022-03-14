13ba67cf2c8c386066962f8e86d03af42b521162
anish <anish.ravichandran@gmail.com>
2022-03-14 12:37:02 +0530

    mar 22

--- database/cart.go
+++ database/cart.go
@@ -1,1 +1,19 @@
 package database
+
+import "errors"
+
+var (
+	ErrCantFindProduct        = errors.New("Cannot find the Product")
+	ErrCantDecodeProduct      = errors.New("Cannot find the Product")
+	ErrInvalidUserID          = errors.New("User is not valid")
+	ErrCantUpdateUser         = errors.New("Cannot update Usert")
+	ErrCantRemoveItemFromCart = errors.New("Cannot remove this Item from the Cart")
+	ErrCantGetItem            = errors.New("Unable to get this Item from the Cart")
+	ErrCantBuyCartItem        = errors.New("Cannot buy this Item from the Cart")
+)
+
+func AddToCart()       {}
+func RemoveItem()      {}
+func GetItemFromCart() {}
+func BuyItemFromCart() {}
+func InstantBuy()      {}
--- database/database_setup.go
+++ database/database_setup.go
@@ -1,1 +1,40 @@
 package database
+
+import (
+	"context"
+	"fmt"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+	"time"
+)
+
+// connects the DB
+func DBSet() *mongo.Client {
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Timeout after given time
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+
+	defer cancel()
+
+	err = client.Connect(ctx) // connect to the DB
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(context.TODO(), nil) //ping to check if connected
+	if err != nil {
+		fmt.Println("Failed to connect to MongoDB")
+		return nil
+	}
+	fmt.Println("Successfully connected to MongoDB")
+	return client
+}
+
+func UserData(client *mongo.Client, collectionName string) *mongo.Collection {}
+
+func ProductData(client *mongo.Client, collectionName string) *mongo.Collection {}
